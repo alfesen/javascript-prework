d0afac8006db48922479aaa6fa6f66d012b92b04
Alexander Fesenko <a.fesenko2812@gmail.com>
2021-09-01 10:52:14 +0200

    Add script for randomNumber (commented + opened) + Module exercise

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 let computerMove ='kamień';
-let playerMove='papier';
-
 printMessage('Zagrałem ' +  computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove='papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+//let randomFraction = Math.random();
+//printMessage('Wylosowany ułamek to ' + randomFraction)
+
+//let calculation= randomFraction * 3+1;
+//printMessage('Ułamek pomnożony przez 3 i powiększ o 1: ' + calculation);
+
+//let roundNumber = Math.floor(calculation);
+//printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
+
+//let randomNumber = Math.floor(Math.random() * 9 + 11);
+//printMessage('Wylosowana liczba: ' + randomNumber);
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('Wylosowana liczba: ' + randomNumber);
