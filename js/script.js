dabda2b6e51745e83acb0df065686ccc71e890b3
Alexander Fesenko <a.fesenko2812@gmail.com>
2021-09-01 16:11:34 +0200

    Add code for computerMove

--- js/script.js
+++ js/script.js
@@ -4,8 +4,12 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
+if(randomNumber == 1) {
   computerMove = 'kamień';
+} else if(randomNumber == 2) {
+    computerMove = 'papier'
+} else if(randomNumber == 3) {
+    computerMove = 'nożyce'
 }
 
 printMessage('Mój ruch to: ' + computerMove);
